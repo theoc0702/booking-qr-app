0a40b6f451e4e037e6aa88e3f841d6f0d1cb17a8
Theo Chelly <chellytheo@gmail.com>
2025-04-08 12:53:33 +0200

    premier commit

---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, StatusBar, Platform } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: 'https://application-mobile.ideaweb.fr/' }} // remplace par ton vrai lien
+        style={styles.webview}
+        javaScriptEnabled
+        domStorageEnabled
+        startInLoadingState
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  webview: {
+    flex: 1,
+  },
+});

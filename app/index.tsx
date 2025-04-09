af1903e069637c4f3b0ebec0e7ad490380b217b3
Theo Chelly <chellytheo@gmail.com>
2025-04-09 22:56:00 +0200

    1.0.0

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, StatusBar, Platform } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { Stack } from 'expo-router';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <WebView
-        source={{ uri: 'https://application-mobile.ideaweb.fr/' }} // remplace par ton vrai lien
-        style={styles.webview}
-        javaScriptEnabled
-        domStorageEnabled
-        startInLoadingState
-      />
-    </SafeAreaView>
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <SafeAreaView style={styles.container}>
+        <WebView
+          source={{ uri: 'https://application-mobile.ideaweb.fr/' }}
+          style={styles.webview}
+          javaScriptEnabled
+          domStorageEnabled
+          startInLoadingState
+        />
+      </SafeAreaView>
+    </>
   );
 }
 
